248d04c36c23a9185d028cb2fc3f362c458470d0
Benjamin Doherty <bjd@pobox.com>
2010-03-09 11:45:28 -0600

    rewrite bookmarklet and script to use jquery ui dialogs.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,111 +1,102 @@
+// Inspired by:
+// http://jqueryui.com/themeroller/developertool/developertool.js.php
+
 "use strict";
-/*global window,document,event,location,DRUPAL_BOOKMARKLET_HOST */
 
-function drupalBookmarklet($, L) {
-  // get the currently selected text
-  var t, body, iframe_url, existing_iframe;
-  try {
-    t = ((window.getSelection && window.getSelection()) || (document.getSelection && document.getSelection()) || (document.selection && document.selection.createRange && document.selection.createRange().text));
-  }
-  catch (e) { // access denied on https sites
-    t = "";
-  }
+/*global window,DRUPAL_BOOKMARKLET_HOST,jQuery */
 
-  body = t.toString();
+var drupalBookmarklet;
+drupalBookmarklet = {};
+drupalBookmarklet.s1 = document.createElement('script');
+drupalBookmarklet.s2 = document.createElement('script');
+drupalBookmarklet.s1.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.js');
+drupalBookmarklet.s2.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/jquery-ui.js');
+document.getElementsByTagName('head')[0].appendChild(drupalBookmarklet.s1);
+document.getElementsByTagName('head')[0].appendChild(drupalBookmarklet.s2);
+//once jq and ui are loaded...
+drupalBookmarklet.s1.onload = function () {
+  drupalBookmarklet.s2.onload = function () {
+    (function ($) {
+      $('<link/>')
+        .attr({
+          href: 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/themes/flick/jquery-ui.css',
+          rel: 'stylesheet',
+          type: 'text/css',
+          media: 'screen'
+        })
+        .appendTo('head');
 
-  if (body === "") {
-    body = "";
-  }
+      // get the currently selected text
+      var t, body, iframe_url, existing_iframe;
+      try {
+        t = ((window.getSelection && window.getSelection()) || (document.getSelection && document.getSelection()) || (document.selection && document.selection.createRange && document.selection.createRange().text));
+      }
+      catch (e) { // access denied on https sites
+        t = "";
+      }
 
-  iframe_url = DRUPAL_BOOKMARKLET_HOST;
-  iframe_url += '/node/add/link?bookmarklet';
+      body = t.toString();
 
-  iframe_url += '&edit[field_link][0][url]=';
-  iframe_url += encodeURIComponent(location.href);
+      if (body === "") {
+        body = "";
+      }
 
-  iframe_url += '&edit[field_link][0][title]=';
-  iframe_url += encodeURIComponent(document.title);
+      iframe_url = DRUPAL_BOOKMARKLET_HOST;
+      iframe_url += '/node/add/link?bookmarklet';
 
-  if (body !== "") {
-    iframe_url += '&edit[body_field][body]=';
-    iframe_url += encodeURIComponent(body);
-  }
+      iframe_url += '&edit[field_link][0][url]=';
+      iframe_url += encodeURIComponent(location.href);
 
-  existing_iframe = $('#drupal_bookmarklet_iframe')[0];
+      iframe_url += '&edit[field_link][0][title]=';
+      iframe_url += encodeURIComponent(document.title);
 
-  if (existing_iframe) {
-    $('#drupal_bookmarklet').show();
-    // if has text selected, copy into iframe
-    if (body !== "") {
-      existing_iframe.src = iframe_url;
-    }
-    else {
-      // want to set focus back to that item! but can't; access denied
-    }
-    return;
-  }
+      if (body !== "") {
+        iframe_url += '&edit[body_field][body]=';
+        iframe_url += encodeURIComponent(body);
+      }
 
-  // wrapper
-  $('<div id="drupal_bookmarklet"/>')
-    .css({
-      position: 'absolute',
-      right: '0px',
-      zIndex: 10000,
-      margin: '10px',
-      top: '0px'
-    })
-    .prependTo('body')
+      existing_iframe = $('#drupal_bookmarklet_iframe')[0];
 
-    // inner
-    .append('<div/>')
-    .children('div')
-    .css({
-      backgroundColor: 'white',
-      zIndex: 2,
-      width: '500px',
-      height: '400px',
-      border: 'solid rgb(180,180,180)',
-      borderWidth: '6px'
-    })
+      if (existing_iframe) {
+        $('#drupal_bookmarklet').show();
+        // if has text selected, copy into iframe
+        if (body !== "") {
+          existing_iframe.src = iframe_url;
+        }
+        else {
+          // want to set focus back to that item! but can't; access denied
+        }
+        return;
+      }
 
-    // iframe
-    .append('<iframe/>')
-    .children('iframe')
-    .attr({
-      src: iframe_url,
-      frameborder: 0,
-      scrolling: 'no',
-      name: 'drupal_bookmarklet_iframe',
-      id: 'drupal_bookmarklet_iframe'
-    })
-    .css({
-      width: '100%',
-      height: '100%',
-      border: '1px',
-      padding: '0px',
-      margin: '0px'
-    })
+      $('<iframe/>')
+        .attr({
+          src: iframe_url,
+          frameborder: 0,
+          scrolling: 'no',
+          name: 'drupal_bookmarklet_iframe',
+          id: 'drupal_bookmarklet_iframe'
+        })
+        .dialog({
+          position: ['right', 'top'],
+          title: 'post to EI'
+        })
+        .css({
+          width: '100%',
+          height: '100%',
+          border: '1px',
+          padding: '0px',
+          margin: '0px'
+        });
 
-    // close button
-    .parent('<div/>')
-    .append('<a/>')
-    .children('a')
-    .css({
-      width: '100%',
-      color: '#ff0000'
-    })
-    .click(function () {
-      $('#drupal_bookmarklet').remove();
-    })
-    .text('x');
-
-  $(document).keypress(function (event) {
-    if (event.keyCode == '27') {
-      event.preventDefault();
-      $('#drupal_bookmarklet').toggle();
-    }
-  });
-
-}
+      $(document).keypress(function (event) {
+        if (event.keyCode === '27') {
+          event.preventDefault();
+          $('#drupal_bookmarklet').toggle();
+        }
+      });
+    }(jQuery.noConflict(true)));
+  };
+};
 
 /*jslint white: true, browser: true, devel: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true, indent: 2 */
