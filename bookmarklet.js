64abf16ae645c1dae805e0890505ec4dd5c35773
Benjamin Doherty <bjd@pobox.com>
2010-03-14 20:25:27 -0500

    Revert "stashing default node type in the jquery data isn't going to work."
    This reverts commit e316de0bc821a70664eab656332dac178835dfa0.

--- bookmarklet.js
+++ bookmarklet.js
@@ -192,7 +192,8 @@ drupalBookmarklet.createBookmarklet = function (buttons, nodeType) {
     .dialog({
       position: ['right', 'top'],
       buttons: buttons
-    });
+    })
+    .data('defaultNodeType', nodeType);
 
   // jQuery UI stylesheets assumes base font size of 11px.
   this.dialog.data('dialog').uiDialog.css({
