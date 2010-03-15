1626c82ac7f41a077d6f22d59df737e21f0bb2d6
Benjamin Doherty <bjd@pobox.com>
2010-03-14 21:42:40 -0500

    whitespace.

--- bookmarklet.child.js
+++ bookmarklet.child.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
   }, 100);
 
 });
+
 Drupal.behaviors.bookmarkletPostMessage = function (context) {
   if (Drupal.settings.bookmarklet.event !== "undefined") {
     $.postMessage(Drupal.settings.bookmarklet.event, Drupal.settings.bookmarklet.target_url);
